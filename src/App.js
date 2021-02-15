2533c16aae3da646063d0bff3f5bbdf33ed768b7
Ryan Williams <r007will@gmail.com>
2021-02-16 02:51:47 +0300

    Navbar complete

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 // import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import Hero from './components/Hero';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
-      </header>
+      <Hero />
     </div>
   );
 }
---
+++ src/components/Hero.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Nav from './Nav';
+
+
+function Hero() {
+    return (
+        <div>
+            <Nav />
+            
+        </div>
+    )
+}
+
+export default Hero
